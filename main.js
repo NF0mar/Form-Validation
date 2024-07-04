63c70b5138e3a770e1b664d857314c17f5585af6
Nur Farah <nuurfaarax15@gmail.com>
2024-07-05 00:13:54 +0300

    chore: Add lazy loading for images and improve form validation

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", () =>{
 const username = document.querySelector(".username");
 const email = document.querySelector(".email");
 const password = document.querySelector(".password");
@@ -90,3 +91,4 @@ const checkPasswordSimilarity = (input) => {
 }
 
 
+})
